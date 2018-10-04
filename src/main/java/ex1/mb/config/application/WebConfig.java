87c2316a401b0d6e2ab707244e68eabb9e62de32
Svetlana <maminyaschik@gmail.com>
2018-10-04 10:10:24 +0300

    feature/MB-0002 add JDBC

--- src/main/java/ex1/mb/config/application/WebConfig.java
+++ src/main/java/ex1/mb/config/application/WebConfig.java
@@ -1,11 +1,12 @@
 package ex1.mb.config.application;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.ComponentScan;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+        import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+        import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan({"ex1.mb"})
@@ -19,5 +20,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         viewResolver.setSuffix(".jsp");
         viewResolver.setContentType("text-html;charset=utf-8");
         return viewResolver;
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/views/css");
     }
 }
--- src/main/java/ex1/mb/controller/AppController.java
+++ src/main/java/ex1/mb/controller/AppController.java
@@ -1,9 +1,9 @@
 package ex1.mb.controller;
 
+import ex1.mb.service.CreateTable;
 import ex1.mb.service.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,13 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppController {
     @Autowired
-  //1.  @Value("first spring bean!")
+    //1.  @Value("first spring bean!")
     private Message message;
 
     @Qualifier("myServiceImpl")
     @Autowired
-   //3. @Qualifier("main")
+    //3. @Qualifier("main")
     private MyService myService;
+
+    @Autowired
+    private CreateTable createTable;
 
    /*1. @Value("second spring bean!")
     private final Word word;
@@ -28,22 +31,29 @@ public class AppController {
         this.word = word;
     }*/
 
-  /*2.  @RequestMapping("/")
-    public String getHomePage(Model model) {
-        model.addAttribute("name", message.getName());
-      //1.  model.addAttribute("name", word.getName());
-        return "home";
-    }*/
-
+    /*2.  @RequestMapping("/")
+      public String getHomePage(Model model) {
+          model.addAttribute("name", message.getName());
+        //1.  model.addAttribute("name", word.getName());
+          return "home";
+      }*/
+// localhost:8080/
     @RequestMapping("/")
     public String getHomePage(Model model) {
         model.addAttribute("name", myService.getName());
-           return "home";
+        return "home";
     }
 
+    // localhost:8080/name/asdasd
     @RequestMapping("/name/{name}")
     public String getName(@PathVariable("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
+    }
+
+    @RequestMapping("/create")
+    public String createTable(Model model) {
+        model.addAttribute("status", createTable.getTableCreationStatus());
+        return "create";
     }
 }
