d41602abe37f9467b27386f49e7afc77e56699aa
Svetlana <maminyaschik@gmail.com>
2018-10-22 18:15:00 +0300

    add test checkGetAll

--- src/test/java/ex1/mb/controller/CatControllerIntegrationTest.java
+++ src/test/java/ex1/mb/controller/CatControllerIntegrationTest.java
@@ -2,11 +2,14 @@ package ex1.mb.controller;
 
 import ex1.mb.entity.Cat;
 import org.junit.Test;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -16,6 +19,7 @@ public class CatControllerIntegrationTest {
     private final static String ROOT = "http://localhost:8080/cat";
     private final static String ADD = "/add";
     private final static String GET_BY_ID = "/get";
+    private static final String ALL ="/all" ;
 
     @Test
     public void addCat() {
@@ -33,6 +37,25 @@ public class CatControllerIntegrationTest {
 
         assertEquals("OK", responseEntity.getStatusCode().getReasonPhrase());
         assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+   public  void checkGetAllCats(){
+        createdCat();
+        createdCat();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Cat>> responseEntity=restTemplate.exchange(
+                ROOT + GET_BY_ID + ALL,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Cat>>() {
+                     }
+        );
+
+       List<Cat> cats=responseEntity.getBody();
+       assertNotNull(cats.get(0));
+       assertNotNull(cats.get(1));
     }
 
     private Cat createdCat() {
